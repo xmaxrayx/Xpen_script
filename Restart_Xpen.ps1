5163a243caf9a60b32563646df1dab3ff845c65e
xMaxrayx <101531362+xmaxrayx@users.noreply.github.com>
2022-12-05 01:44:11 +0300

    Update Restart_Xpen.ps1

--- Restart_Xpen.ps1
+++ Restart_Xpen.ps1
@@ -1,3 +1,4 @@
 TASKKILL /IM PenTablet.exe
 Start-Process "C:\Program Files\Pentablet\PenTablet.exe" /mini
+Start-Sleep -Milliseconds 999
 Get-Process PenTablet | Set-WindowState -State FORCEMINIMIZE
